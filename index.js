355c682dab1882f3895b57084b735f1d48728db2
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-12-17 19:33:37 +0530

    Complete API Joke Data Fetch & to show on client side

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World from Node JS");
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}.`);
-})
+app.get("/", async (req, res) => {
+  // res.render("index.ejs");
+  try {
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Programming");
+    const result = response.data;
+    console.log(result);
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
